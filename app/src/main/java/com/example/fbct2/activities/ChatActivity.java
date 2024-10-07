c73ec8d0945115eba22724b73baea916a64233a0
JuanAlfonso11 <Juan.alfonso.alvarado@hotmail.com>
2024-10-06 22:03:28 -0400

    commit inicial app firebase

---
+++ app/src/main/java/com/example/fbct2/activities/ChatActivity.java
@@ -0,0 +1,22 @@
+package com.example.fbct2.activities;
+
+import static com.example.fbct2.R.*;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fbct2.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/com/example/fbct2/listeners/UserListener.java
@@ -0,0 +1,10 @@
+package com.example.fbct2.listeners;
+
+import com.example.fbct2.models.User;
+
+
+public interface UserListener {
+    void onUserClicked(User user);
+
+}
+
